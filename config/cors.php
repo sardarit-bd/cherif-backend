851248353df905dc8b974c011e13d02f564ec660
Ahsan Alam <ahsansfiverr@gmail.com>
2025-10-23 15:48:20 +0600

    cors update

--- config/cors.php
+++ config/cors.php
@@ -7,7 +7,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000' ],
+    'allowed_origins' => ['https://cherifai.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
