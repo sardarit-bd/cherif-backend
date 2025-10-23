9cf4ebf90883e00fd4920c30586e26a68d6c1b5c
Ahsan Alam <ahsansfiverr@gmail.com>
2025-10-23 11:41:54 +0600

    role based model initialized

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,13 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/cc',function(){
+    Artisan::call('config:cache');
+    Artisan::call('route:cache');
+    Artisan::call('view:cache');
+    return 'successfully caches cleared';
+});
+
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login',    [AuthController::class, 'login']);
