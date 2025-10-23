a5fd086828ba572d6191ff0d7c2e6f337064ad70
Ahsan Alam <ahsansfiverr@gmail.com>
2025-10-23 11:40:34 +0600

    role based model initialized

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
+
+
+Route::get('/cc',function(){
+    Artisan::call('config:cache');
+    Artisan::call('route:cache');
+    Artisan::call('view:cache');
+    return 'successfully caches cleared';
+});
 
 Route::get('/', function () {
     return view('welcome');
