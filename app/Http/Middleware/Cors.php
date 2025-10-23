de2865d5f016e21cf24b4f8deb24713cd3359c86
Ahsan Alam <ahsansfiverr@gmail.com>
2025-10-23 17:32:49 +0600

    git commit custom cors middleware

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Cors
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization');
+
+        return $response;
+    }
+}
