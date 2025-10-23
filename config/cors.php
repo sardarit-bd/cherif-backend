ef92429c87b85082ec9932e73481e0ba1d2d61ec
Ahsan Alam <ahsansfiverr@gmail.com>
2025-10-23 12:55:06 +0600

    cors update

--- config/cors.php
+++ config/cors.php
@@ -2,8 +2,8 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://cherifai.vercel.app', 'http://localhost:3000'],
+    'allowed_origins' => ['*'],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['Authorization'],
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
